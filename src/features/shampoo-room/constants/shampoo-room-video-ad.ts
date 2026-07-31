b291413b16e6a7e7f91982261cc4695667286eb2
Saehanseul <saehanseul@gmail.com>
2026-07-31 15:11:15 +0900

    feat: 샴푸실광고 교체

--- src/features/shampoo-room/constants/shampoo-room-video-ad.ts
+++ src/features/shampoo-room/constants/shampoo-room-video-ad.ts
@@ -1,9 +1,9 @@
 export const SHAMPOO_ROOM_VIDEO_AD = {
   id: 'shampoo-room-youtube-video-ad',
   thumbnailUrl: '/shampoo-room-video-ad-thumbnail.jpg',
-  ctaUrl: 'https://www.visualacademy.co.kr/visualonly/?idx=203',
+  ctaUrl: 'https://www.visualacademy.co.kr/hair/?idx=193',
   videoEmbedUrl:
-    'https://www.youtube.com/embed/w3r63eGWzXM?autoplay=1&playsinline=1&rel=0&si=Rd76_ogg5HRgDFaK',
+    'https://www.youtube.com/embed/pyGu_4kWni8?autoplay=1&playsinline=1&rel=0',
 } as const;
 
 export const SHAMPOO_ROOM_VIDEO_AD_BASE_EVENT_PROPERTIES = {
